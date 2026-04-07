025bdd5d244c0ac352c116e021ff7b380dd4c72c
amarnath0038 <amarnathreddy841@gmail.com>
2026-04-07 07:04:01 +0530

    Added new font

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,6 +1,13 @@
 import { Outfit } from "next/font/google";
+import { Jost } from "next/font/google";
+
 
 export const outfit = Outfit({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
+
+export const jost = Jost({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
 });
