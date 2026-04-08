bb2c862fc3076cc09df7bda79b3a0e0fdcd2e646
amarnath0038 <amarnathreddy841@gmail.com>
2026-04-08 10:47:03 +0530

    Added work section

---
+++ src/components/WorkSection.tsx
@@ -0,0 +1,53 @@
+// import FacesFrames from "./FacesFrames";
+
+// const WorkSection = () => {
+//   return (
+//     <section>
+//       <FacesFrames />
+//     </section>
+//   );
+// };
+
+// export default WorkSection;
+
+
+import ImageScroller from "./ImageScroller";
+
+const WorkSection = () => {
+  const r2Limages = [
+    "/R2LSwiperImage1.webp",
+    "/R2LSwiperImage2.webp",
+    "/R2LSwiperImage3.webp",
+  ];
+
+  const l2Rimages = [
+    "/L2RSwiperImage1.webp",
+    "/L2RSwiperImage2.webp",
+    "/L2RSwiperImage3.webp",
+  ];
+
+  return (
+    <section className="bg-[#fff3e9]">
+      
+      {/* Header */}
+      <div className="px-6 sm:px-8 md:px-16 pt-10 sm:pt-20">
+        <div className="flex justify-center text-center mb-10">
+          <li className="text-black text-base md:text-lg font-medium mb-3 sm:mb-5">
+            WORK THAT SPEAKS FOR ITSELF
+          </li>
+        </div>
+      </div>
+      
+      <div className="flex flex-col gap-11 pb-10">
+        {/*R2L Slider */}
+        <ImageScroller images={r2Limages} direction="right" speed={20} />
+
+        {/* L2R Slider */}
+        <ImageScroller images={l2Rimages} direction="left" speed={20} />
+      </div>
+
+    </section>
+  );
+};
+
+export default WorkSection;
