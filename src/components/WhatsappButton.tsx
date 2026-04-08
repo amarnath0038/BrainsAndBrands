74bc77eb402d77f5b524b925d16662e535d8b61c
amarnath0038 <amarnathreddy841@gmail.com>
2026-04-08 16:30:23 +0530

    Added whatsapp button

---
+++ src/components/WhatsappButton.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+const WhatsAppButton = () => {
+  return (
+    <a
+      href="https://wa.me/918983225507?text=Hi%20I’d%20like%20to%20know%20more"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="fixed right-6 bottom-[15px] z-[9999]"
+    >
+      <Image
+        src="/whatsapp-logo-2.svg"
+        alt="WhatsApp"
+        width={48}
+        height={48}
+        className="drop-shadow-lg hover:scale-105 transition-transform duration-200"
+      />
+    </a>
+  );
+};
+
+export default WhatsAppButton;
