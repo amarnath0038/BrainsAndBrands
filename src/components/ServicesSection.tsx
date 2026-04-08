18d59c3cc95f4b9daea49f1dc58c5180acc8c7fd
amarnath0038 <amarnathreddy841@gmail.com>
2026-04-08 10:46:10 +0530

    Added services section

---
+++ src/components/ServicesSection.tsx
@@ -0,0 +1,13 @@
+import ServicesDesktop from "./ServicesDesktop";
+import ServicesMobile from "./ServicesMobile";
+
+const ServicesSection = () => {
+  return (
+    <>
+      <ServicesDesktop />
+      <ServicesMobile />
+    </>
+  );
+};
+
+export default ServicesSection;
